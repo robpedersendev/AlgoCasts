646af78444d29918265ff1fe13426de21e0f7df4
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2019-10-01 22:50:31 -0600

    Added solutions to reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,10 +7,25 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  //   debugger;
+  // Solution #1
+  //   return str.split('').reverse().join('')
+
+  // Solution #2
+  //   let reversed = "";
+  //   for (let character of str) {
+  //     reversed = character + reversed;
+  //   }
+  //   return reversed;
+
+  // Solution #3.1 ES 5.1
+  //   return str.split("").reduce((reversed, character) => {
+  //     return character + reversed;
+  //   }, "");
+
+  // Solution #3.2 ES 2015+
+  debugger;
   return str.split("").reduce((rev, char) => char + rev, "");
 }
 
-// reverse("apples");
-
+reverse("asdf");
 module.exports = reverse;
